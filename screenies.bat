a51d4b0d676de7a58da72c2e8a7477e5cf1940f6
crowview <jezterlemad@gmail.com>
2016-11-24 20:04:39 -0600

    Update screenies.bat

--- screenies.bat
+++ screenies.bat
@@ -4,13 +4,12 @@ rem 	for screencapping genga/douga credits
 set dirpath=.\folderthatholdsanimevideos
 set credtime=23:57
 set append=_genga
-set i=0
 echo Working...
 for %%f in ("%dirpath%\*.*") do (
     set "infile=%%~nf"
     set "extn=%%~xf"
     setlocal ENABLEDELAYEDEXPANSION
-    ffmpeg -v error -ss %credtime% -i "%dirpath%\!infile!!extn!" -vframes 1 -y "!infile!"%append%.jpg 
-    if not exist "!infile!"%append%.jpg echo Failed to create !infile!%append%.jpg
+    ffmpeg -v error -ss %credtime% -i "%dirpath%\!infile!!extn!" -vframes 1 -y "!infile!%append%".jpg 
+    if not exist "!infile!%append%".jpg echo Failed to create !infile!%append%.jpg
     endlocal
 )
