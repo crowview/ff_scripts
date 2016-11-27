3fcbb1e6e6305f2b78fd56e6c819e86e387e72ef
crowview <jezterlemad@gmail.com>
2016-11-26 21:21:03 -0600

    Update probe.bat

--- probe.bat
+++ probe.bat
@@ -4,5 +4,5 @@ IF "%1"=="" (
 ECHO Usage: probe path\to\video
 ECHO takes a single video argument and tells you the width, height, pixel ratio, and display ratio of each video stream, ignoring non-video streams
 ) ELSE (
-ffprobe -v error -show_entries stream=index,codec_type,width,height,sample_aspect_ratio,display_aspect_ratio,r_frame_rate,avg_frame_rate,bit_rate -select_streams v -unit "%1"
+ffprobe -v error -show_entries format=size:width,height,sample_aspect_ratio,display_aspect_ratio,r_frame_rate,avg_frame_rate,bit_rate -select_streams v -unit "%1"
 )
